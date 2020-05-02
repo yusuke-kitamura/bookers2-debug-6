757bb2422a2a0948ca7cfc4b04a049e2d55554d8
vagrant <vagrant@localhost.localdomain>
2020-05-02 07:12:44 +0000

    gemfileにbootstrap-sass追記

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,7 +40,4 @@ Rails.application.configure do
 
   # Print deprecation notices to the stderr.
  	 config.active_support.deprecation = :silence
-
-  # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
   devise_for :users
+
   root 'home#top'
   get 'home/about'
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
 end
