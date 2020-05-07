58d9390c0135562034a7ba313484e159b89a8672
vagrant <yusuke1209kitamura@gmail.com>
2020-05-07 08:03:14 +0000

    [update]fontawsome埋め込み

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   root 'home#top'
   get 'home/about'
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books do
+  	resource :favorites,only: [:create,:destroy]
+  end
+
 end
