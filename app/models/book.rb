8d20a654a90dacfb3fc3038c98f235dd036bc732
vagrant <yusuke1209kitamura@gmail.com>
2020-05-07 07:19:18 +0000

    [update]favoriteモデル作成し各モデルとアソシエーション

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,11 @@
 class Book < ApplicationRecord
 	belongs_to :user
+	has_many :favorites, dependent: :destroy
 	attachment :profile_image
+	def favorited_by?(user)
+            favorites.where(user_id: user.id).exists?
+    end
+
 	#バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
 	#presence trueは空欄の場合を意味する。
 	validates :title, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable,:validatable
 
   has_many :books
+  has_many :favorites, dependent: :destroy:
   attachment :profile_image, destroy: false
 
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
