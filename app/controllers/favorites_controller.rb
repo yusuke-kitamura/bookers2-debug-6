7a92d4878b5b788a981b4d1de8112e4e6a27076b
vagrant <yusuke1209kitamura@gmail.com>
2020-05-07 08:51:34 +0000

    [update]favoritesコントローラーにアクション記述

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,15 @@
 class FavoritesController < ApplicationController
 	def create
+		@book = Book.find(params[:book_id])
+		favorite = @book.favorites.new(user_id: current_user.id)
+		favorite.save!
+		redirect_back(fallback_location: root_path)
 	end
 
 	def destroy
+		@book = Book.find(params[:book_id])
+		favorite = current_user.favorites.find_by(book_id: @book.id)
+		favorite.destroy
+		redirect_back(fallback_location: root_path)
 	end
 end
